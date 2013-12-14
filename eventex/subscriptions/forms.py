87a5190eec95c7671f495fae3989b8d5d5c39eea
Biguá <bigua.kun@gmail.com>
2013-12-14 18:42:28 -0200

    telefone
    telefone com ddd em campo separado

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from django.utils.translation import ugettext as _
 from django.core.exceptions import ValidationError
 from eventex.subscriptions.models import Subscription
+from django.core.validators import EMPTY_VALUES
 
 
 def CPFValidator(value):
@@ -12,7 +13,40 @@ def CPFValidator(value):
         raise ValidationError(_(u'CPF deve ter 11 números'))
 
 
+class PhoneWidget(forms.MultiWidget):
+
+    def __init__(self, attrs=None):
+        widgets = (
+            forms.TextInput(attrs=attrs),
+            forms.TextInput(attrs=attrs))
+        super(PhoneWidget, self).__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if not value:
+            return [None, None]
+        return value.split('-')
+
+
+class PhoneField(forms.MultiValueField):
+    widget = PhoneWidget
+
+    def __init__(self, *args, **kwargs):
+        fields = (forms.IntegerField(),
+                  forms.IntegerField())
+        super(PhoneField, self).__init__(fields, *args, **kwargs)
+
+    def compress(self, data_list):
+        if not data_list:
+            return ''
+        if data_list[0] in EMPTY_VALUES:
+            raise forms.ValidationError(_(u'DDD inválido.'))
+        if data_list[1] in EMPTY_VALUES:
+            raise forms.ValidationError(_(u'Número inválido.'))
+        return '%s-%s' % tuple(data_list)
+
+
 class SubscriptionForm(forms.ModelForm):
+    phone = PhoneField(label=_('Telefone'), required=False)
 
     class Meta:
         model = Subscription
